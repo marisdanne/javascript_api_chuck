ce58f795b61711a4976064f66240a5c6aa908f56
Maris Danne <maris.danne@gmail.com>
2020-10-21 15:44:01 +0300

    ar listeneri

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ async function getJoke(){
     let paragrafs = document.getElementById('joke')
     paragrafs.innerHTML = joksJson.value
 }
+
+
+window.addEventListener('load', function(){
+    let jokaPoga = document.getElementById('getJoke')
+
+    jokaPoga.addEventListener('click', async function(){
+        let atbilde = await fetch("https://api.chucknorris.io/jokes/random")
+        let joksJson = await atbilde.json()
+    
+        let paragrafs = document.getElementById('joke')
+        paragrafs.innerHTML = joksJson.value
+    })
+})
